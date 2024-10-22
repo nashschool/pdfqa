33160cd41d1500910b2e7f5add1df9a4690aa3e1
nashschool <info@nashschool.com>
2024-10-22 16:03:22 +0100

    new api - stepwise

--- app/main.py
+++ app/main.py
@@ -19,10 +19,12 @@ from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.runnables.history import RunnableWithMessageHistory
 
-import os
-
 # Configure logging
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', handlers=[logging.StreamHandler()])
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    handlers=[logging.StreamHandler()],
+)
 
 app = FastAPI()
 
@@ -42,6 +44,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 @app.exception_handler(RequestValidationError)
 async def validation_exception_handler(request: Request, exc: RequestValidationError):
     logging.error(f"Validation error for {request.url}: {exc.errors()}")
@@ -51,6 +54,60 @@ async def validation_exception_handler(request: Request, exc: RequestValidationE
     )
 
 
+@app.post("/step-evaluator")
+async def step_evaluator(file: UploadFile = File(...)):
+    try:
+        file_content = await file.read()
+        question, correct_answer, prev_answer, curr_answer = (
+            extract_question_and_answer_conv_from_pdf(file_content)
+        )
+        step_evaluator_template = """You are an examiner who provides detailed feedback and fair marking.
+
+        Question:
+        {question}
+
+        Correct Answer:
+        {correct_answer}
+
+        User's current step:
+        {user_curr_step}
+
+        User's previous steps:
+        {user_prev_steps}
+
+        Address the user on their current step of the answer and see if they are building on their previous steps, and by
+        detailing the solution till the point of the current step, encourage them to get to the next step and the correct answer. 
+        Enclose all math inside \( ... \)
+
+        Language: The user can also enter the chat in other languages like Hindi or Hinglish or Tamil, etc, 
+        in which case you must respond in the same language as the user. 
+        
+        Keep your guidance concise.
+        If the user has the correct answer, your feedback must have:
+        - Marks out of 10 (based on the current and previous steps )
+        - 3-line feedback addressed to the user
+        For irrelevant answer, you must only say Irrelevant in the feeback."""
+
+        formula_extractor = RAGSetup(
+            step_evaluator_template,
+            ["question", "correct_answer", "user_curr_step", "user_prev_steps"],
+        )
+        rag_chain = formula_extractor.create_rag_chain()
+        feedback = rag_chain.invoke(
+            {
+                "question": question,
+                "correct_answer": correct_answer,
+                "user_curr_step": curr_answer,
+                "user_prev_steps": prev_answer,
+            }
+        )
+        return JSONResponse(content={"answer": feedback})
+
+    except Exception as e:
+        logging.error(f"An error occurred: {e}", exc_info=True)
+        return JSONResponse(content={"error": str(e)}, status_code=500)
+
+
 @app.post("/formula-extractor")
 async def formula_extractor(file: UploadFile = File(...)):
     try:
@@ -58,35 +115,45 @@ async def formula_extractor(file: UploadFile = File(...)):
         question, correct_answer = extract_question_and_answer_from_pdf(file_content)
         formula_extractor_template = """
         You are a formula extractor. Given a question and a correct answer,
-        explain the mathematical formulas used in solving the question. The formulas must be generic and the notation must be explained.
-        Example Answer: Formula 1. Distance formula: \(D = sqrt((x2-x1)^2+(y2-y1)^2)\) between points \((x1,y1)\) and \((x2,y2)\). Formula 2.
-        No need to allude to the specific problem and its numbers. 
+        explain the mathematical formulas used in solving the question. The formulas must be not too elementary (like Pythagoras 
+        theorem or \(sin^2(\theta)+cos^2(\theta)=1\).). Instead they must be informative to a sixth form student and the notation must be explained.
+        Don't allude to the specific problem and its numbers. 
         Ensure to enclose mathematical symbols and equations inside \(...\).
-        Just list the formulas one by one; no intro or conclusion or additional
-        explanation is necessary.
+        Just list the formulas one by one; don't give any introductory line or conclusion or additional
+        explanation.
+        Follow the Example Answer template: 
+        
+        Formula 1. Distance formula: \(D = sqrt((x2-x1)^2+(y2-y1)^2)\) between points \((x1,y1)\) and \((x2,y2)\). 
+        Formula 2. Sine addition \(\sin(A+B)=\sin A \cos B + \cos A \sin B \) 
+        
         Question:
         {question}
         Correct Answer:
         {correct_answer}"""
 
-        formula_extractor = RAGSetup(formula_extractor_template, ["question", "correct_answer"])
+        formula_extractor = RAGSetup(
+            formula_extractor_template, ["question", "correct_answer"]
+        )
         rag_chain = formula_extractor.create_rag_chain()
-        feedback = rag_chain.invoke({
-            "question": question,
-            "correct_answer": correct_answer,
-        })
+        feedback = rag_chain.invoke(
+            {
+                "question": question,
+                "correct_answer": correct_answer,
+            }
+        )
         return JSONResponse(content={"answer": feedback})
-    
+
     except Exception as e:
         logging.error(f"An error occurred: {e}", exc_info=True)
         return JSONResponse(content={"error": str(e)}, status_code=500)
-    
+
+
 @app.post("/check-answer")
 async def check_ans(file: UploadFile = File(...), user_answer: str = Form(...)):
     try:
         file_content = await file.read()
         question, correct_answer = extract_question_and_answer_from_pdf(file_content)
-        prompt ="""You are an examiner who provides detailed feedback and fair marking.
+        prompt = """You are an examiner who provides detailed feedback and fair marking.
 
         Question:
         {question}
@@ -96,32 +163,38 @@ async def check_ans(file: UploadFile = File(...), user_answer: str = Form(...)):
 
         User's Answer:
         {user_answer}
+        
+        Language: The user can also enter the answer in other languages like Hindi or Hinglish or Tamil, etc, 
+        in which case you have to respond in the same language as the user. 
 
-        Please compare the user's answer with the correct answer. Your answer must have:
+        Please evaluate the user's answer by comparing with the correct answer. Your answer must have:
         - Marks out of 10
-        - 3-line feedback addressed to the user
-        For irrelevant answer, you must only say Irrelevant in the feeback. Ensure to enclose mathematical symbols 
+        - 3-line encouraging feedback that must be addressed to the user, 
+        For irrelevant answer, say it is Irrelevant and nudge them to get back to study. Ensure to enclose mathematical symbols 
         and equations inside \(...\)."""
-       
-        answer_checker = RAGSetup(prompt, ["question", "correct_answer",  "user_answer"])
+
+        answer_checker = RAGSetup(prompt, ["question", "correct_answer", "user_answer"])
         rag_chain = answer_checker.create_rag_chain()
-        feedback = rag_chain.invoke({
-            "question": question,
-            "correct_answer": correct_answer,
-            "user_answer": user_answer
-        })
+        feedback = rag_chain.invoke(
+            {
+                "question": question,
+                "correct_answer": correct_answer,
+                "user_answer": user_answer,
+            }
+        )
         return JSONResponse(content={"answer": feedback})
 
     except Exception as e:
         logging.error(f"An error occurred: {e}", exc_info=True)
         return JSONResponse(content={"error": str(e)}, status_code=500)
 
+
 @app.post("/ask-question")
 async def ask_question(file: UploadFile = File(...), user_question: str = Form(...)):
     try:
         file_content = await file.read()
         question, correct_answer = extract_question_and_answer_from_pdf(file_content)
-        prompt="""You are an assistant for question-answering tasks. 
+        prompt = """You are an assistant for question-answering tasks. 
         You are given a question and an answer. You have to answer follow-up question on the 
         question and answer. 
         Question:
@@ -129,19 +202,25 @@ async def ask_question(file: UploadFile = File(...), user_question: str = Form(.
         Correct Answer:
         {correct_answer}
         User Question: {user_question} 
-        You can only make conversations based on the provided information and should 
-        refrain from making assumptions. If information isn’t available in 
-        context to answer, politely say you don’t have knowledge about that. 
+        You should try to answer the user's question based on the 
+        provided question and the correct answer. You can use your knowledge to answer relevant questions which
+        are not fully clarifiable using the given information.
+        Language: The user can also enter the chat in other languages like Hindi or Hinglish or Tamil, etc, 
+        in which case you have to respond in the same language as the user. 
+        Refrain from making assumptions. 
         If it is a related mathematical question, explain the answer in detail, 
-        using steps where necessary. Ensure to enclose mathematical symbols and equations inside \(...\)."""
-        
+        using steps where necessary. Ensure to enclose mathematical symbols and equations inside \(...\)
+        Politely refuse irrelevant questions or comments, urging them to get back to study."""
+
         ask_ques = RAGSetup(prompt, ["question", "correct_answer", "user_question"])
         rag_chain = ask_ques.create_rag_chain()
-        ai_clarification = rag_chain.invoke({
-            "question": question,
-            "correct_answer": correct_answer,
-            "user_question": user_question
-        })
+        ai_clarification = rag_chain.invoke(
+            {
+                "question": question,
+                "correct_answer": correct_answer,
+                "user_question": user_question,
+            }
+        )
 
         return JSONResponse(content={"answer": ai_clarification})
 
@@ -149,9 +228,11 @@ async def ask_question(file: UploadFile = File(...), user_question: str = Form(.
         logging.error(f"An error occurred: {e}", exc_info=True)
         return JSONResponse(content={"error": str(e)}, status_code=500)
 
+
 # Post-processing
 def format_docs(docs):
     return "\n\n".join(doc.page_content for doc in docs)
+
 
 def setup_retriever(doc_splits):
     embedding = OpenAIEmbeddings(model="text-embedding-3-large")
@@ -160,21 +241,20 @@ def setup_retriever(doc_splits):
 
 
 def gen(llm, retriever, input_question):
-    
-    print(f'ques is: {input_question}')
+
+    print(f"ques is: {input_question}")
     retriever = Retriever(doc_splits, embeddings)
 
     generation = conversational_rag_chain.invoke(
         {"input": input_question},
-        config={
-            "configurable": {"session_id": "abc123"}
-        },  
+        config={"configurable": {"session_id": "abc123"}},
     )["answer"]
     return generation
 
+
 def gen_with_history(llm, retriever, input_question):
-    
-    print(f'ques is: {input_question}')
+
+    print(f"ques is: {input_question}")
     contextualize_q_system_prompt = (
         "Given a chat history and the latest user question "
         "which might reference context in the chat history, "
@@ -199,7 +279,7 @@ def gen_with_history(llm, retriever, input_question):
         "If you think the question is irrelevant, say that you "
         "don't know. If it is mathematical, answer in steps."
     )
-    
+
     qa_prompt = ChatPromptTemplate.from_messages(
         [
             ("system", system_prompt),
@@ -227,11 +307,10 @@ def gen_with_history(llm, retriever, input_question):
     )
     generation = conversational_rag_chain.invoke(
         {"input": input_question},
-        config={
-            "configurable": {"session_id": "abc123"}
-        },  
+        config={"configurable": {"session_id": "abc123"}},
     )["answer"]
     return generation
+
 
 def extract_question_and_answer_from_pdf(file_content):
     """
@@ -244,15 +323,42 @@ def extract_question_and_answer_from_pdf(file_content):
         docs = [PyPDFLoader(tmp_file.name).load()]
         text = "\n".join([doc.page_content for d in docs for doc in d])
         # Extract the question and answer
-        question_start = text.find('Question:')   
-        answer_start = text.find('Solution:')
+        question_start = text.find("Question:")
+        answer_start = text.find("Solution:")
         if question_start != -1 and answer_start != -1:
-            question = text[question_start + len('Question:'):answer_start].strip()
-            correct_answer = text[answer_start + len('Solution:'):].strip()
+            question = text[question_start + len("Question:") : answer_start].strip()
+            correct_answer = text[answer_start + len("Solution:") :].strip()
         else:
-            raise ValueError("Could not find 'Question:' or 'Solution:' markers in the PDF.") 
+            raise ValueError(
+                "Could not find 'Question:' or 'Solution:' markers in the PDF."
+            )
         return question, correct_answer
-        
+
+
+def extract_question_and_answer_conv_from_pdf(file_content):
+    """
+    Extracts the question and correct answer from a PDF file.
+    Assumes the PDF contains 'Question:' and 'Answer:' as markers.
+    """
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as tmp_file:
+        tmp_file.write(file_content)
+        tmp_file.flush()
+        docs = [PyPDFLoader(tmp_file.name).load()]
+        text = "\n".join([doc.page_content for d in docs for doc in d])
+        # Extract the question and answer
+        question_start = text.find("Question:")
+        answer_start = text.find("Solution:")
+        prev_step = text.find("Prev Step:")
+        curr_step = text.find("Current Step:")
+        if question_start != -1 and answer_start != -1:
+            question = text[question_start + len("Question:") : answer_start].strip()
+            correct_answer = text[answer_start + len("Solution:") :].strip()
+        else:
+            raise ValueError(
+                "Could not find 'Question:' or 'Solution:' markers in the PDF."
+            )
+        return question, correct_answer, prev_step, curr_step
+
 
 def process_document(file_content):
     with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as tmp_file:
@@ -262,6 +368,7 @@ def process_document(file_content):
         docs_list = [item for sublist in docs for item in sublist]
         text_splitter = RecursiveCharacterTextSplitter(chunk_size=300, chunk_overlap=0)
         return text_splitter.split_documents(docs_list)
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
